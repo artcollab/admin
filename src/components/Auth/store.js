7ecee84483c78405075d2b043c30e327c7c4a507
Yoav Levi <13078659+koaie@users.noreply.github.com>
2022-03-25 23:30:15 +0000

    init

---
+++ src/components/Auth/store.js
@@ -0,0 +1,44 @@
+import { writable } from 'svelte/store'
+export const user = writable(null)
+export const authenticating = writable(true)
+export const logout = () => {
+  localStorage.removeItem('user')
+  authenticate()
+}
+
+export const login = (username, password) => {
+  authenticating.set(true)
+  if (username === 'user@example.com' && password === 'pass') {
+    const user = { username, token: 'abcdefg' }
+    storeCredentials(user)
+    // we're delaying authentication to simulate realworld timings
+    setTimeout(authenticate, 500)
+  } else {
+    authenticating.set(false)
+    console.error('wrong username or password')
+  }
+}
+
+// we're delaying authentication to simulate realworld timings
+setTimeout(authenticate, 500)
+
+function authenticate() {
+  /**
+   * Insecure example!
+   * In production, a token should be stored in localStorage/cookie
+   * and sent to an auth server for verification.
+   * */
+  user.set(getCredentials())
+
+  // we need to inform other components that we're no longer authenticating
+  authenticating.set(false)
+}
+
+function getCredentials() {
+  const cred = localStorage.getItem('user')
+  return cred && JSON.parse(cred)
+}
+
+function storeCredentials(user) {
+  localStorage.setItem('user', JSON.stringify(user))
+}
---
+++ src/pages/_layout.svelte
@@ -0,0 +1,39 @@
+<script>
+  import NavBar from '../components/Navbar/Bar.svelte'
+
+  import Link from '../components/Auth/Link.svelte'
+</script>
+
+<div id="app">
+  <aside>
+    <div>
+      <Link />
+    </div>
+    <NavBar />
+  </aside>
+  <section>
+    <header />
+    <main class="container">
+      <slot />
+    </main>
+    <footer />
+  </section>
+</div>
+
+<style>
+  #app {
+    display: grid;
+    grid-template-columns: 196px auto;
+    height: 100%;
+    width: 100%;
+    position: absolute;
+  }
+  main {
+    padding-top: 64px;
+  }
+  aside {
+    padding: 16px;
+    box-shadow: 0 0 16px rgba(0, 0, 0, 0.2);
+    height: 100%;
+  }
+</style>
