c9d6bbb96dac75e1833562ba64184a0288fd5f1d
Yoav Levi <13078659+koaie@users.noreply.github.com>
2022-03-25 23:59:07 +0000

    add alias, smui

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,30 @@
 <script>
   import { Router } from '@roxi/routify'
-  import { routes } from '../.routify/routes'
+  import { routes } from '@routify/routes'
 </script>
 
 <Router {routes} />
 
 <style global>
 </style>
+
+<svelte:head>
+  <!-- Fonts -->
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+  />
+
+  <!-- Material Typography -->
+  <link
+    rel="stylesheet"
+    href="https://unpkg.com/@material/typography@13.0.0/dist/mdc.typography.css"
+  />
+
+  <!-- SMUI -->
+  <link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+</svelte:head>
