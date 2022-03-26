ab74775652b036c536fb027d3f2366b8b3ec0a24
Yoav Levi <13078659+koaie@users.noreply.github.com>
2022-03-26 20:16:04 +0000

    fallback

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -1,1 +1,32 @@
-<h1>404</h1>
+<script>
+  import Button, { Label } from "@smui/button";
+  import Card, { Content, Actions } from "@smui/card";
+  import { goto } from "@roxi/routify";
+</script>
+
+<div class="fallback">
+  <Card>
+    <Content
+      ><div class="mdc-typography--body1">
+        Whoops something went wrong!
+      </div></Content
+    >
+    <Actions fullBleed>
+      <Button
+        on:click={() => {
+          $goto(history.back());
+        }}
+      >
+        <Label><slot name="action">Go back</slot></Label>
+        <i class="material-icons" aria-hidden="true">arrow_forward</i>
+      </Button>
+    </Actions>
+  </Card>
+</div>
+
+<style lang="scss">
+  @import "index.scss";
+  .fallback {
+    margin-top: 3vh;
+  }
+</style>
