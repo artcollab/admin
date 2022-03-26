667340a899ab8256666d3fc6679ae4d466e7b127
Yoav Levi <13078659+koaie@users.noreply.github.com>
2022-03-26 20:31:44 +0000

    global top margin

--- src/pages/_fallback.svelte
+++ src/pages/_fallback.svelte
@@ -4,29 +4,24 @@
   import { goto } from "@roxi/routify";
 </script>
 
-<div class="fallback">
-  <Card>
-    <Content
-      ><div class="mdc-typography--body1">
-        Whoops something went wrong!
-      </div></Content
+<Card>
+  <Content
+    ><div class="mdc-typography--body1">
+      Whoops something went wrong!
+    </div></Content
+  >
+  <Actions fullBleed>
+    <Button
+      on:click={() => {
+        $goto(history.back());
+      }}
     >
-    <Actions fullBleed>
-      <Button
-        on:click={() => {
-          $goto(history.back());
-        }}
-      >
-        <Label><slot name="action">Go back</slot></Label>
-        <i class="material-icons" aria-hidden="true">arrow_forward</i>
-      </Button>
-    </Actions>
-  </Card>
-</div>
+      <Label><slot name="action">Go back</slot></Label>
+      <i class="material-icons" aria-hidden="true">arrow_forward</i>
+    </Button>
+  </Actions>
+</Card>
 
 <style lang="scss">
   @import "index.scss";
-  .fallback {
-    margin-top: 3vh;
-  }
 </style>
