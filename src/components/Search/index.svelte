5b8f1d9d7beccf0040523c2a361d2bf3a8371e21
Yoav Levi <13078659+koaie@users.noreply.github.com>
2022-03-27 12:59:40 +0100

    Search comp

---
+++ src/components/Search/index.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import Textfield from "@smui/textfield";
+  import Icon from "@smui/textfield/icon";
+  import HelperText from "@smui/textfield/helper-text";
+
+  export let icon = "search";
+  export let text = "search";
+  let search = "";
+
+  export let data: any[] = ["Hello", "Hi"];
+  export let results: any[] = [];
+  export let searchEl = (el) => {
+    return el as string;
+  };
+  $: if (results) {
+    console.log(results);
+  }
+
+  $: if (search) {
+    results = [];
+    data.forEach((el) => {
+      if (searchEl(el).toLowerCase().includes(search.toLowerCase())) {
+        results.push(el);
+      }
+    });
+  }
+</script>
+
+<div>
+  <Textfield variant="filled" bind:value={search} label="Trailing Icon">
+    <Icon class="material-icons" slot="trailingIcon">{icon}</Icon>
+    <HelperText slot="helper">{text}</HelperText>
+  </Textfield>
+</div>
+
+<style lang="scss">
+  @import "index.scss";
+</style>
