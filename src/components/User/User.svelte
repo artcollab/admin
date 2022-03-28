11db0b9db6290badcdd8967270fb464f62729aa6
Yoav Levi <13078659+koaie@users.noreply.github.com>
2022-03-28 14:41:21 +0100

    users done

--- src/components/User/User.svelte
+++ src/components/User/User.svelte
@@ -10,6 +10,7 @@
   import HelperText from "@smui/textfield/helper-text";
   import FormField from "@smui/form-field";
   import Checkbox from "@smui/checkbox";
+  import Button, { Label } from "@smui/button";
   import { auth } from "#auth/store";
 
   export let user: User;
@@ -19,7 +20,7 @@
       id: user.id,
       email: user.email,
       username: user.username,
-      profileID: user.profileID,
+      profileID: "",
       name: "",
       surname: "",
     };
@@ -41,9 +42,28 @@
         console.log(err);
       });
   };
-  const save = () => {
+  const save = async () => {
     user.name = _user.name ? _user.name : user.name;
     user.surname = _user.surname ? _user.surname : user.surname;
+    user.profileID = _user.profileID ? _user.profileID : user.profileID;
+    await auth()
+      .put(`${import.meta.env.VITE_API_URL}/users/edit/${user.username}`, {
+        json: {
+          name: user.name,
+          surname: user.surname,
+          profileID: user.profileID,
+          admin: user.admin,
+        },
+      })
+      .json()
+      .then((res) => {
+        if (user) {
+          action();
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 </script>
 
@@ -54,7 +74,7 @@
         <Cell span={2}>
           <Avatar name="{user.name} {user.surname}" />
         </Cell>
-        <Cell span={6}>
+        <Cell span={3}>
           <h2 class="mdc-typography--headline6" style="margin: 0;">
             {user.email}
           </h2>
@@ -62,8 +82,14 @@
             {user.username}
           </h3>
         </Cell>
-        <Cell span={4}>
+        <Cell span={6}>
           <div class="right">
+            <IconButton
+              class="material-icons"
+              on:click={() => {
+                save();
+              }}>save</IconButton
+            >
             <IconButton
               class="material-icons"
               on:click={() => {
@@ -89,15 +115,20 @@
           </Textfield>
         </Cell>
         <Cell span={6}>
+          <Textfield
+            type="name"
+            bind:value={_user.profileID}
+            label={user.profileID}
+          >
+            <Icon class="material-icons" slot="leadingIcon">perm_identity</Icon>
+            <HelperText slot="helper">Profile name</HelperText>
+          </Textfield>
+        </Cell>
+        <Cell span={6}>
           <FormField>
             <Checkbox bind:checked={user.admin} />
             <span slot="label"> admin </span>
           </FormField>
-        </Cell>
-        <Cell span={6}>
-          <div class="right">
-            <IconButton class="material-icons">save</IconButton>
-          </div>
         </Cell>
       </LayoutGrid>
     {:else}
